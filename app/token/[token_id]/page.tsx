39afbef3e0e03190ca4b22e34add90cd67faef7c
JonathanKimchi <59551654+JonathanKimchi@users.noreply.github.com>
2023-11-20 21:25:08 -0500

    Removing function export from interview page

--- app/token/[token_id]/page.tsx
+++ app/token/[token_id]/page.tsx
@@ -14,7 +14,7 @@ const acceptedTokens = [
     '6',
     '7',
 ];
-export async function isAcceptedToken(tokenId: string) {
+async function isAcceptedToken(tokenId: string) {
     return acceptedTokens.includes(tokenId);
 }
 
